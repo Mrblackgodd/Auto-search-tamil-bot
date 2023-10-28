f629e013ee09d6db44af014ebeb6302394196df1
Mrblackgodd <136906179+Mrblackgodd@users.noreply.github.com>
2023-10-28 08:52:32 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -552,11 +552,11 @@ async def get_verify_shorted_link(link):
                         return data["shortlink"]
                     else:
                         logger.error(f"Error: {data['message']}")
-                        return f'https://{URL}/direct_link?api_key={API}&pages=3&link={link}'
+                        return f'https://{URL}/direct_link?api_key={API}&pages=3&link={link}={direct_link}'
 
         except Exception as e:
             logger.error(e)
-            return f'https://{URL}/direct_link?api_key={API}&pages=3&link={link}'
+            return f'{URL}/direct_link?api_key={API}&pages=3&link={link}={direct_link}'
     else:
         url = f'https://{URL}/api'
         params = {'api': API,
@@ -570,11 +570,11 @@ async def get_verify_shorted_link(link):
                         return data['shortenedUrl']
                     else:
                         logger.error(f"Error: {data['message']}")
-                        return f'https://{URL}/api?api={API}&link={link}'
+                        return f'https://{URL}/api?api={API}&link={link}={direct_link}'
 
         except Exception as e:
             logger.error(e)
-            return f'{URL}/api?api={API}&link={link}'
+            return f'{URL}/api?api={API}&link={link}={direct_link}'
 
 async def check_token(bot, userid, token):
     user = await bot.get_users(userid)
